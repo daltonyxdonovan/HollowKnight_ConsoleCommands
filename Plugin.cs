bb9e3bd5227620331052a99fcad97659284bfad3
Dalton Donovan <90434265+daltonyxdonovan@users.noreply.github.com>
2023-05-29 17:50:21 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using UnityEngine;
+using Unity;
+
+namespace ConsoleCommands
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                PlayerData.instance.AddHealth(1);
+            }
+        }
+    }
+}
