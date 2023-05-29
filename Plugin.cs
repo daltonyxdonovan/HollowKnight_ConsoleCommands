c65d15c7d00167cd25bcc722dc8467d6e8ff9530
Dalton Donovan <90434265+daltonyxdonovan@users.noreply.github.com>
2023-05-29 18:03:17 -0500

    bepinex test

--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,16 @@
 ﻿using BepInEx;
 using UnityEngine;
 using Unity;
+using TMPro;
 
 namespace ConsoleCommands
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        Canvas canvas;
+        TextMeshProUGUI popup_text;
+
         private void Awake()
         {
             // Plugin startup logic
@@ -15,9 +19,28 @@ namespace ConsoleCommands
 
         public void Update()
         {
+            if (canvas == null)
+            {
+                //create a new canvas
+                canvas = new GameObject("Canvas", typeof(Canvas)).GetComponent<Canvas>();
+                //create popup_text and set it's parent to canvas
+                popup_text = new GameObject("Popup Text", typeof(TextMeshProUGUI)).GetComponent<TextMeshProUGUI>();
+                popup_text.transform.SetParent(canvas.transform);
+                //set the text to be in the center of the screen
+                popup_text.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+                popup_text.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+                popup_text.rectTransform.anchoredPosition = new Vector2(0, 0);
+                popup_text.rectTransform.sizeDelta = new Vector2(1920, 1080);
+                popup_text.alignment = TextAlignmentOptions.Center;
+                popup_text.fontSize = 2;
+                popup_text.color = Color.red;
+                popup_text.text = "daltonyx <3";
+
+            }
             if (Input.GetKey(KeyCode.Space))
             {
                 PlayerData.instance.AddHealth(1);
+
             }
         }
     }
