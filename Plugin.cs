61b6400eb4de44f0716c6df26bd60076caa9301f
Dalton Donovan <90434265+daltonyxdonovan@users.noreply.github.com>
2023-05-30 12:34:50 -0500

    remove useless using directives refactor current hacks into methods add basic command string parser structure add /health, /mp, /ach_get commands

--- Plugin.cs
+++ Plugin.cs
@@ -15,6 +15,8 @@ namespace ConsoleCommands
         Canvas canvas;
         TextMeshProUGUI popup_text;
         TextMeshProUGUI command_text;
+        string command_string = "";
+
         int popup_timer = 0;
         bool selected = false;
 
@@ -22,6 +24,39 @@ namespace ConsoleCommands
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public void AddToMPReserve(int amount)
+        {
+            PlayerData.instance.MPReserveMax += amount;
+            PlayerData.instance.MPReserveCap += amount;
+            PlayerData.instance.MPReserve += amount;
+            PlayerData.instance.AddToMaxMPReserve(amount);
+            Log($"Added {amount} to MP Reserve\nMP = {PlayerData.instance.MPReserve}\nMaxMPreserve = {PlayerData.instance.MPReserveMax}");
+        }
+
+        public void AddToHealthReserve(int amount)
+        {
+            PlayerData.instance.maxHealthBase += amount;
+            PlayerData.instance.maxHealth += amount;
+            PlayerData.instance.AddHealth(amount);
+            PlayerData.instance.AddToMaxHealth(amount);
+            Log($"Added {amount} to Health Reserve\nHealth = {PlayerData.instance.health}\nMaxHealth = {PlayerData.instance.maxHealth}");
+        }
+
+        public void UnlockAllAchievements()
+        {
+            Log("Unlocking All Achievements\nRate the mod on nexus if you enjoy! (;");
+            //find current achievement handler
+            AchievementHandler achievementHandler = FindObjectOfType<AchievementHandler>();
+
+            //I'm not sure which one actually worked, but I'm leaving both in because I don't wanna test it. It works, and that's all that matters. (it gave me all 63 achievements <3)
+            achievementHandler.AwardAllAchievements();
+
+            foreach (Achievement achievement in achievementHandler.achievementsList.achievements)
+            {
+                achievementHandler.AwardAchievementToPlayer(achievement.key);
+            }
         }
 
         public void Log(string message)
@@ -63,35 +98,20 @@ namespace ConsoleCommands
                 command_text.transform.SetParent(canvas.transform);
                 command_text.rectTransform.anchorMin = new Vector2(0, 0);
                 command_text.rectTransform.anchorMax = new Vector2(0, 0);
-                command_text.rectTransform.anchoredPosition = new Vector2(-13,-8);
+                command_text.rectTransform.anchoredPosition = new Vector2(0,0);
                 command_text.alignment = TextAlignmentOptions.Center;
                 command_text.fontSize = .5f;
                 command_text.color = Color.white;
                 command_text.text = "Press / for commands";
+                selected = false;
+                command_string = "";
+                //set position to (-12,8)
+                command_text.rectTransform.position = new Vector3(-12.5f, -8, 0);
 
                 //bring it to the front of the 2d screen
                 canvas.planeDistance = 1;
                 //set it to the 'UI' layer
                 canvas.gameObject.layer = 5;
-
-                
-
-
-                
-                
-
-
-                
-
-
-                
-                
-
-
-
-
-
-
             }
             if (canvas != null)
             {
@@ -105,41 +125,91 @@ namespace ConsoleCommands
                     }
                 }
 
+                if (Input.GetKeyDown(KeyCode.Slash))
+                {
+                    if (selected)
+                    {
+                        selected = false;
+                        command_text.text = "Press / for commands";
+                        command_string = "";
+                    }
+                    else
+                    {
+                        selected = true;
+                        command_text.text = "/";
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.Backspace))
+                {
+                    if (selected)
+                    {
+                        if (command_string.Length > 0)
+                        {
+                            command_string = command_string.Substring(0, command_string.Length - 1);
+                            command_text.text = "/" + command_string;
+                        }
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    if (selected)
+                    {
+                        selected = false;
+                        command_text.text = "Press / for commands";
+                        command_string = "";
+                    }
+                }
+
 
                 if (Input.GetKey(KeyCode.Return))
                 {
-                    Log("Added to max MP reserve.");
-                    PlayerData.instance.MPReserveMax += 10;
-                    PlayerData.instance.MPReserveCap += 10;
-                    PlayerData.instance.MPReserve += 10;
-                    PlayerData.instance.AddToMaxMPReserve(10);
-                    Log($"MP = {PlayerData.instance.MPReserve}\nMaxMPreserve = {PlayerData.instance.MPReserveMax}");
-                }
 
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    Log("added to health");
-                    PlayerData.instance.maxHealthBase += 10;
-                    PlayerData.instance.maxHealth += 10;
-                    PlayerData.instance.AddHealth(10);
-                    PlayerData.instance.AddToMaxHealth(10);
-                    Log($"Health = {PlayerData.instance.health}\nMaxHealth = {PlayerData.instance.maxHealth}");
-                }
-
-                if (Input.GetKey(KeyCode.F1))
-                {
-                    Log("Unlocking All Achievements");
-                    //find current achievement handler
-                    AchievementHandler achievementHandler = FindObjectOfType<AchievementHandler>();
-
-                    //I'm not sure which one actually worked, but I'm leaving both in because I don't wanna test it. It works, and that's all that matters.
-                    achievementHandler.AwardAllAchievements();
-
-                    foreach (Achievement achievement in achievementHandler.achievementsList.achievements)
+                    if (command_string.StartsWith("/health"))
                     {
-                        achievementHandler.AwardAchievementToPlayer(achievement.key);
+                        string[] strings = command_text.text.Split();
+                        int choice = int.Parse(strings[1]);
+                        
+                        if (choice < 0)
+                        {
+                            Log("You can't add negative health! (;");
+                        }
+                        else
+                        {
+                            AddToHealthReserve(choice);
+                        }
                     }
+
+                    else if (command_string.StartsWith("/mp"))
+                    {
+                        string[] strings = command_text.text.Split();
+                        int choice = int.Parse(strings[1]);
+
+                        if (choice < 0)
+                        {
+                            Log("You can't add negative MP! (;");
+                        }
+                        else
+                        {
+                            AddToMPReserve(choice);
+                        }
+                    }
+
+                    else if (command_string.StartsWith("/ach_get"))
+                    {
+                        UnlockAllAchievements();
+                    }
+
+
+
+
+
                 }
+
+                
+
+                
             }
         }
     }
